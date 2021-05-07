3521819c0aa10b0e48b200a4b9ac2d3a0d0e7fb7
zaebee <zaebuntu@gmail.com>
2021-05-08 01:55:24 +0200

    Sorted memes by Timestamp.

--- store/index.ts
+++ store/index.ts
@@ -20,7 +20,7 @@ export const mutations: MutationTree<RootState> = {
 
 export const actions: ActionTree<RootState, RootState> = {
   async fetchMemes({ commit }) {
-    const memes = await $http.get('/memes/_search')
+    const memes = await $http.post('/memes/_search', {sort: {Timestamp: {order: 'desc'}}})
     console.log(memes.data.hits.hits)
     commit('SET_MEMES', memes.data.hits.hits)
     commit('CHANGE_NAME', 'New name')
